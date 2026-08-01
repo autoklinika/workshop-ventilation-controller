3a74ed6413d5c5ee8ec111764b1499a01c944355
autoklinika <86266304+autoklinika@users.noreply.github.com>
2026-08-01 11:54:09 +0200

    feat: add RS-485 bring-up package

---
+++ src/ventilation_core/rs485/__init__.py
@@ -0,0 +1,21 @@
+"""RS-485 / Modbus RTU bring-up infrastructure."""
+
+from .modbus import (
+    ModbusCRCError,
+    ModbusError,
+    ModbusExceptionResponse,
+    append_crc,
+    build_read_holding_registers_request,
+    crc16_modbus,
+    parse_read_holding_registers_response,
+)
+
+__all__ = [
+    "ModbusCRCError",
+    "ModbusError",
+    "ModbusExceptionResponse",
+    "append_crc",
+    "build_read_holding_registers_request",
+    "crc16_modbus",
+    "parse_read_holding_registers_response",
+]
