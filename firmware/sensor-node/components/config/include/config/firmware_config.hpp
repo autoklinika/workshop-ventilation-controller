10ca18c22a09666e72f82cd842087fbc0a11ac51
autoklinika <86266304+autoklinika@users.noreply.github.com>
2026-08-06 15:49:40 +0200

    Version OTA bootstrap firmware as 0.5.0

--- firmware/sensor-node/components/config/include/config/firmware_config.hpp
+++ firmware/sensor-node/components/config/include/config/firmware_config.hpp
@@ -5,8 +5,8 @@
 namespace config::firmware {
 
 inline constexpr char kProjectName[] = "kamod-sen55-sensor-node";
-inline constexpr char kVersion[] = "0.4.0-stage1";
-inline constexpr std::uint16_t kFirmwareVersionPacked = 0x0004;
+inline constexpr char kVersion[] = "0.5.0-stage1";
+inline constexpr std::uint16_t kFirmwareVersionPacked = 0x0005;
 
 inline constexpr std::uint32_t kApplicationLoopPeriodMs = 100;
 inline constexpr std::uint32_t kSensorPollPeriodMs = 200;
