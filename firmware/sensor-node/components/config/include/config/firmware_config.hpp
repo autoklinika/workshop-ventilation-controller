ccd467581e614995d836732334a7231f5f6ae2ff
autoklinika <86266304+autoklinika@users.noreply.github.com>
2026-08-06 17:20:27 +0200

    firmware: prepare 0.5.1 first OTA test image

--- firmware/sensor-node/components/config/include/config/firmware_config.hpp
+++ firmware/sensor-node/components/config/include/config/firmware_config.hpp
@@ -5,7 +5,7 @@
 namespace config::firmware {
 
 inline constexpr char kProjectName[] = "kamod-sen55-sensor-node";
-inline constexpr char kVersion[] = "0.5.0-stage1";
+inline constexpr char kVersion[] = "0.5.1-stage1";
 inline constexpr std::uint16_t kFirmwareVersionPacked = 0x0005;
 
 inline constexpr std::uint32_t kApplicationLoopPeriodMs = 100;
