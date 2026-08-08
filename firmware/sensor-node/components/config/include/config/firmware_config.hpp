71f8f857032846e7d8cef0849f46bc6bbcb4f18d
autoklinika <86266304+autoklinika@users.noreply.github.com>
2026-08-08 11:11:21 +0200

    firmware: identify rollback-test image in service version

--- firmware/sensor-node/components/config/include/config/firmware_config.hpp
+++ firmware/sensor-node/components/config/include/config/firmware_config.hpp
@@ -2,10 +2,16 @@
 
 #include <cstdint>
 
+#include "sdkconfig.h"
+
 namespace config::firmware {
 
 inline constexpr char kProjectName[] = "kamod-sen55-sensor-node";
+#ifdef CONFIG_WVC_OTA_ROLLBACK_TEST_IMAGE
+inline constexpr char kVersion[] = "0.5.2-stage1-rollback-test";
+#else
 inline constexpr char kVersion[] = "0.5.1-stage1-fix1";
+#endif
 inline constexpr std::uint16_t kFirmwareVersionPacked = 0x0005;
 
 inline constexpr std::uint32_t kApplicationLoopPeriodMs = 100;
@@ -14,6 +20,7 @@ inline constexpr std::uint32_t kSensorReconnectPeriodMs = 5'000;
 inline constexpr std::uint32_t kFirstMeasurementTimeoutMs = 10'000;
 inline constexpr std::uint32_t kMeasurementStaleAfterMs = 5'000;
 inline constexpr std::uint32_t kOtaConfirmationDelayMs = 30'000;
+inline constexpr std::uint32_t kOtaRollbackTestRestartDelayMs = 15'000;
 inline constexpr std::uint32_t kMaximumConsecutiveReadErrors = 3;
 
 inline constexpr std::uint32_t kModbusBaudRate = 19'200;
