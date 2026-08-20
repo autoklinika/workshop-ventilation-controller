d882adf1d64071b896aed6d442e9729124eecb0c
autoklinika <86266304+autoklinika@users.noreply.github.com>
2026-08-20 13:02:13 +0200

    hmi: add device admin receiver

---
+++ hmi/iiyama-android/app/src/main/java/pl/autoklinika/workshopventilation/hmi/KioskDeviceAdminReceiver.java
@@ -0,0 +1,10 @@
+package pl.autoklinika.workshopventilation.hmi;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Device policy controller entry point used to provision this application as
+ * Android Device Owner on the dedicated iiyama HMI.
+ */
+public final class KioskDeviceAdminReceiver extends DeviceAdminReceiver {
+}
